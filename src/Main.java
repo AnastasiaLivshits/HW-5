73f53da1a71ddc80168cbc9d4a745423e67384de
AnastasiaLivshits <nastasia.arch@gmail.com>
2024-08-31 22:41:54 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,82 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Task1");
         int clientOs = 0;
-        if (clientOs == 1) {
+        if (clientOs == 0)
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
+        else
             System.out.println("Установите версию приложения для Android по ссылке");
+
+        System.out.println("Task2");
+        int clientDeviceYear = 2015;
+        if (clientOs == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию iOs по ссылке");
+        } else if (clientOs == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию iOs по ссылке");
+        } else if (clientOs == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию Android по ссылке");
+        } else {
+            System.out.println("Установите обычную версию Android по ссылке");
         }
-    }
-
-    {
-        {
-            System.out.println("Task2");
-            int clientIos = 0;
-            int clientAndroid= 1;//0 для iOS, 1 для Android
-            int clientDeviceYear = 2013;
-
-            if (clientDeviceYear<2015 && clientAndroid==1) {
-                System.out.println("Установите облегченную версию для Android по ссылке");
-            }else if (clientDeviceYear>= 2015) {
-                System.out.println("E");
-            }else if (clientDeviceYear<2015 && clientIos==0){
-                System.out.println("T");}
-            else {
-                System.out.println("RR");}}}}
-
-
+        System.out.println("Task3");
+        int year = 1584;
+        if (year > 1584 && (year % 4 == 0 && year % 100 != 0)) {
+            ;
+            System.out.println(year + "Год является високосным");
+        } else {
+            System.out.println(year + "Год не является високосным");
+        }
+        System.out.println("Task4");
+        int distance = 120;
+        if (distance <= 20) {
+            System.out.println("если расстояние равно " + distance + " км, доставка занимает сутки");
+        } else if (distance > 20 && distance <= 60) {
+            System.out.println("если расстояние равно " + distance + " км, доставка занимает 2 дня");
+        } else if (distance <= 100 && distance > 60) {
+            System.out.println("если расстояние равно " + distance + " км, доставка занимает 3 дня");
+        } else {
+            System.out.println("доставки нет");
+        }
+        System.out.println("Task5");
+        char monthNumber = 4;
+                switch (monthNumber) {
+            case 1:
+                System.out.println("Январь - месяц зимы");
+                break;
+            case 2:
+                System.out.println("Февраль - месяц зимы");
+                break;
+            case 3:
+                System.out.println("Март - месяц весны");
+                break;
+            case 4:
+                System.out.println("Апрель - месяц весны");
+                break;
+            case 5:
+                System.out.println("Май - месяц весны");
+                break;
+            case 6:
+                System.out.println("Июнь - месяц лета");
+                break;
+            case 7:
+                System.out.println("Июль - месяц лета");
+                break;
+            case 8:
+                System.out.println("Август - месяц лета");
+                break;
+            case 9:
+                System.out.println("Сентябрь - месяц осени");
+                break;
+            case 10:
+                System.out.println("Октябрь - месяц осени");
+                break;
+            case 11:
+                System.out.println("Ноябрь - месяц осени");
+                break;
+            case 12:
+                System.out.println("Декабрь - месяц зимы");
+                break;
+            default:
+                System.out.println("Unexpected value");
+        }
+    }};
 
